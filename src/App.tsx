75623225cde69d36e21d37e9504e2bc16613a2bb
nsanjose <kiko.nsj@gmail.com>
2024-01-09 20:37:57 +0800

    test/login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
+import Login from "./Login";
 
-function App() {}
+function App() {
+  return <Login />;
+}
 
 export default App;
