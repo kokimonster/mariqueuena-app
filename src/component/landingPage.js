3cf99ad484b2b1829e0df655901d875e333c16c6
nsanjose <kiko.nsj@gmail.com>
2024-01-11 00:12:43 +0800

    landing features

--- src/component/landingPage.js
+++ src/component/landingPage.js
@@ -1,21 +1,41 @@
-import { Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Container } from "react-bootstrap";
+import {Form,Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import '../App.css';
+
 
 function LandingPageApp () {
   return (  
-    <div className="App">
-    <header className="App-header">
-      <h1>Welcome to Our Website</h1>
-      <p>
-        Explore amazing features and benefits!
-      </p>
-      <div className="d-flex justify-content-center">
-        <a href="#signup" className="btn btn-primary mx-2">Sign Up</a>
-        <a href="#login" className="btn btn-outline-primary mx-2">Login</a>
-      </div>
-    </header>
-  </div>
+    <Navbar className=" justify-content-between"  style={{backgroundColor: "#231099"}}>
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src={require('../img/mrkna.png')}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+              alt="Marikina Logo"
+            />
+          </Navbar.Brand>
+          <div className='d-inline-block text-center text-light'> 
+          <h1 className='text-xl'>MARIQUEUENA</h1>
+          <h6>COMELEC MARIKINA</h6>
+          </div>
+          <Navbar.Brand href="/">
+            <img
+              src={require('../img/comelec.png')}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+              alt="Comelec Logo"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      
   );
 }
 export default LandingPageApp
