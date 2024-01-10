d73b90342986fc92e041296d9fc874b5bf74fdd6
nsanjose <kiko.nsj@gmail.com>
2024-01-10 19:26:28 +0800

    navigation

--- src/component/loginPage.js
+++ src/component/loginPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Form,Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 
 function LoginPage() {
   return (
-    <>
+    <div className='form-container'>
+      <h2>Login Form</h2>
     <Form style={{ maxWidth: '400px', margin: '50px auto', padding: '20px', border: '1px solid #ced4da', borderRadius: '5px', backgroundColor: '#fff', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label style={{ fontWeight: 'bold' }}>Email address</Form.Label>
@@ -19,8 +21,12 @@ function LoginPage() {
       <Button style={{ width: '100%', marginTop: '15px' }}variant="primary" type="submit">
         Login
       </Button>  
+
+      <Link to="/">
+        <button>Back to Home</button>
+      </Link>
     </Form>
-    </>
+    </div>
   );
 }
 
