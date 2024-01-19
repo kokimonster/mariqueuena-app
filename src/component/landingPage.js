be5c511624e05404979b660faef7b91db564327c
rnb <iralphbaluyot@gmail.com>
2024-01-20 00:35:19 +0800

    Landing Page (Admin) now queues the last queued number.

--- src/component/landingPage.js
+++ src/component/landingPage.js
@@ -4,6 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Container, Card, Row, Col } from "react-bootstrap";
 import {Form,Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import Swal from 'sweetalert2';
 import '../App.css';
 
 // // admin view queue
@@ -31,49 +32,65 @@ const Queue = ({ people, joinQueue, serveNext }) => {
   const firstPerson = people.length > 0 ? people[0] : null;
 
   return (
-    <div>
-      <h1>YOUR QUEUE NUMBER IS:</h1>
-      <ul>
-        {firstPerson && (
-          <div className='mt-3'>{firstPerson}</div>
-        )}
-      </ul>
-      <Button variant="success" onClick={joinQueue}>
-        Join the Line
-      </Button>{' '}
-      <Button variant="primary" onClick={serveNext}>
-        Serve Next
-      </Button>
-    </div>
-  );
-};
+    
+      <div>
+        {/* ternary if else to display no queue */}
+        {firstPerson ? (
+          <>
+            <h1>YOUR QUEUE NUMBER IS:</h1>
+            <ul>
+              {firstPerson && (
+                <div className='mt-3'>{firstPerson}</div>
+              )}
+            </ul>
+          </>
+        ) : <h1>No Queue</h1>}
 
-// page background
-const containerStyle = {
-  background: 'linear-gradient(31deg, rgba(251,229,15,1) 0%, rgba(156,135,76,1) 12%, rgba(121,101,98,1) 17%, rgba(35,16,153,1) 50%, rgba(113,93,103,1) 86%, rgba(182,161,59,1) 97%, rgba(251,229,15,1) 100%)',
-  backgroundSize: 'cover',  // Set background size to cover the entire container
-  backgroundPosition: 'center',  // Center the background image
-  backgroundRepeat: 'no-repeat',  // Prevent background image from repeating
-  height: '100vh',
+        <Button variant="success" onClick={joinQueue}>
+          Join the Line
+        </Button>{' '}
+        <Button variant="primary" onClick={serveNext}>
+          Serve Next
+        </Button>
+      </div>
+    
+    
+  );
 };
 
 function LandingPageApp () {
  
   const [peopleInLine, setPeopleInLine] = useState([]);
   const [servedPerson, setServedPerson] = useState(null);
+  const [lastQueue, setLastQueue] = useState(1);
+  const [estimatedTime, setEstimatedTime] = useState(null);
+  const [timeoutId, setTimeoutId] = useState(null);
+
 
   // Function to join the waiting line
   const joinWaitingLine = () => {
-    const newPerson = `${peopleInLine.length + 1}`;
+
+    // const newPerson = `${peopleInLine.length + 1}`;    
+    const newPerson = lastQueue.toString();
     setPeopleInLine([...peopleInLine, newPerson]);
+    setLastQueue(lastQueue + 1);
+    // console.log(`
+    // new person: ${newPerson}
+    // last queue: ${lastQueue}
+    // people in lineJWLL: ${peopleInLine[0]}`);
   };
 
   // Function to serve the next person in line
   const serveNextPerson = () => {
-    if (peopleInLine.length > 0) {
+    if (peopleInLine.length > 0) {    
       const served = peopleInLine[0];
       setServedPerson(served);
       setPeopleInLine(peopleInLine.slice(1));
+      
+      // console.log(`served : ${served}`);
+      // console.log(`people in line: ${peopleInLine}`);
+    } else {
+
     }
   };
 
@@ -85,17 +102,19 @@ function LandingPageApp () {
     }
 
     const baseDuration = 3; // 3 minutes
-    const incrementPerUser = 1; // Increment duration for each user
+    const incrementPerUser = 2; // Increment duration for each user
     const maxDuration = 5; // Maximum waiting time (in minutes) before incrementing
     const numUsers = peopleInLine.length;
-    let estimatedTime = baseDuration + numUsers * incrementPerUser;
+    let calculatedTime  = baseDuration + (numUsers * incrementPerUser);
 
     // if (estimatedTime > maxDuration) {
     //   estimatedTime += 0.5 * baseDuration; // Add 50% of base duration if it exceeds the maximum
     // }
 
-    return estimatedTime;
+    return calculatedTime;
   };
+
+
 
   return (  
     <div className="landingPageStyle">
@@ -131,6 +150,8 @@ function LandingPageApp () {
           <Container>
             <Row style={{ height: '100%' }}>
               <Col md={12} className="mb-4 d-flex flex-column justify-content-between">
+
+                {/* Serving */}
                 <Card className="mt-4 flex-grow-1" style={{ backgroundColor: 'rgba(255, 215, 0, 0.57)', color: '#000000' }}>
                   <Card.Body className='text-center' style={{ fontSize: '4em', fontWeight: 'bold', marginBottom: '20px' }}>
                   <div>
@@ -138,25 +159,26 @@ function LandingPageApp () {
                       </div>
                         {servedPerson && (
                         <div className="mt-3 "> {servedPerson}
-                      </div>
+                        </div>
                     )}
                   </Card.Body>
                 </Card>
                 
+                {/* Queue */}
                 <Card className="mt-4 flex-grow-1 d-flex align-items-center justify-content-center" style={{ backgroundColor: 'rgba(255, 255, 255, 0.57)', color: '#000000', height: '100%' }}>
                   <Card.Body className='text-center' style={{ fontSize: '4em', fontWeight: 'bold', marginBottom: '20px' }}>
-                      {/* display queue */}
-                      {/* {servedPerson ? servedPerson.number : 'No Queue'} */}
-                  <Queue people={peopleInLine} joinQueue={joinWaitingLine} serveNext={serveNextPerson} />
+                    <Queue people={peopleInLine} joinQueue={joinWaitingLine} serveNext={serveNextPerson} />
                   </Card.Body>
                 </Card>
-
+                
+                {/* Estimation Time */}
                 <Card className="mt-4 flex-grow-1" style={{ backgroundColor: 'rgba(255, 215, 0, 0.57)', color: '#000000',  }}>
                   {/* position: 'absolute', bottom: 0, width: '68%' */}
                     <Card.Body className='text-center'>
                       <Card.Title>FROM THIS POINT</Card.Title>
                         <Card.Text>
-                          ESTIMATED WAITING TIME: {calculateEstimatedTime()}
+                          ESTIMATED WAITING TIME: {calculateEstimatedTime() !== null ? (
+                            `${calculateEstimatedTime()} MINUTES`) : <p></p>}
                           {/* ESTIMATED WAITING TIME: {calculateEstimatedTime() !== null ? '${calculateEstimatedTime()} minutes' : ''} */}
                         </Card.Text>
                     </Card.Body>
